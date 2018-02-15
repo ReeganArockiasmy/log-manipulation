23f141a54997d1688216ac0bd7e4c517dbf7a6c2
user <reeganf@gmail.com>
2018-02-15 16:41:07 +0530

    grep function added

--- logmanipulation.sh
+++ logmanipulation.sh
@@ -4,24 +4,25 @@ FROMDATE=""
 TODATE=""
 FROMTIME="00:00:00"
 TOTIME="59:59:59"
+PATTERN=""
+
+
 USAGE="USAGE:\n
 $0 -f <filename> \n
 $0 -f <filename> -d 5,7\n
+$0 -f <filename> -p <pattern> \n
 example:\n
-$0 -f log.txt -d 9,9 -t 14:08:56,14:09:40"
+$0 -f log.txt -d 9,9 -t 14:08:56,14:09:40\n
+$0 -f log.txt -d 9,9 -t 14:08:56,14:09:40 -p V.*"
 
 filter_date(){
     FROMDATE=`echo $1 | cut -d "," -f 1`
     TODATE=`echo $1 | cut -d "," -f 2`
-    echo $FROMDATE
-    echo $TODATE
 }
 
 filter_time(){
     FROMTIME=`echo $1 | cut -d "," -f 1`
     TOTIME=`echo $1 | cut -d "," -f 2`
-    echo $FROMTIME
-    echo $TOTIME
 }
 
 
@@ -42,16 +43,23 @@ do
     then
 	shift
 	LOGFILE=$1
+	
 	#It is check date and save date in date field
     elif [ "$1" = "-d" ]
     then
 	shift
 	filter_date $1
-
+	
     elif [ "$1" = "-t" ]
     then
 	shift
 	filter_time $1
+
+	#It is extract pattern and store the pattern variable
+    elif [ "$1" = "-p" ]
+    then
+    shift
+    PATTERN=$1
     fi
     shift
 done
@@ -59,11 +67,10 @@ done
 # Check log file give to input or not
 if [[ -n $LOGFILE ]]
 then
-    echo "if conditon"
-    awk -v start_day=$FROMDATE -v stop_day=$TODATE -v start_time=$FROMTIME -v stop_time=$TOTIME 'start_day <= $2 && $2 <= stop_day && (start_time <= $3 || start_day != $2) && $3 <= stop_time' $LOGFILE
+    awk -v start_day=$FROMDATE -v stop_day=$TODATE -v start_time=$FROMTIME -v stop_time=$TOTIME 'start_day <= $2 && $2 <= stop_day && (start_time <= $3 || start_day != $2) && $3 <= stop_time' $LOGFILE | grep "$PATTERN"
+    
 else
     echo "must enter file name"
     echo $USAGE
 fi
-
 
